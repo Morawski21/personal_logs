5a53d36b33bfd205bccb77d911b92e011d26d612
Jakub Morawski <morawski.grk@gmail.com>
2025-02-13 08:52:48 +0100

    Add a preview of a local excel file in the app

--- app.py
+++ app.py
@@ -1,6 +1,10 @@
 import streamlit as st
 import pandas as pd
+import random
 
-st.write('Hello!')
+st.title("Hello Synology!")
+st.write(f"Here's your random number: {random.randint(1, 100)}")
 
-df = pd.read_excel("C:\Users\Jakub\Desktop\Logbook 2025")
+df = pd.read_excel("C:\\Users\\Jakub\\Desktop\\Logbook 2025.xlsx")
+
+st.dataframe(df.head())
